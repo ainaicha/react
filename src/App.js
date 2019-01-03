c8767476b54c3c728e5272b6e431cf44d0bdfe34
奶茶 <35134199+ainaicha@users.noreply.github.com>
2019-01-03 16:51:48 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            你好
+          </p>
+          <div>
+              现在是props.data
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
